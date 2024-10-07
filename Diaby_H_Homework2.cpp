e9a9eb77615d15e3a9f4ab15ce305b22f3795c88
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2024-10-06 20:54:50 -0400

    asdasd
    asdsadasd

--- Diaby_H_Homework2.cpp
+++ Diaby_H_Homework2.cpp
@@ -17,17 +17,10 @@ using namespace std;
 
 */ 
 
-
-
-
 int main(){
 
 int Month;
 int day;
-
-
-
-
 
 cout << "Please input the month (enter a number from 1-12 corresponding with the 12 months of the year)" <<endl;
 cin >> Month;
@@ -36,8 +29,25 @@ cout <<"Please input the day of the month (1-31)"<<endl;
 cin >> day;
 
 
-if (Month >=3 && Month <=6 &&  21 <= 20)
-    cout <<"ITS SPRING YIPPPPEEE!!!!!!!!!!"<<endl;
+if (Month >=3 && Month <=6)
+    if(day <=21 )
+        cout <<"It is now spring. 🌸🌺"<<endl;
+
+if (Month >=6 && Month <=9)
+    if(day <=22)
+        cout <<" It is now summer☀️"<< endl;
+
+if (Month>=9 && Month<=12)
+    if (day <=22)
+        cout<<"It is now fall. 🍁🍂"<<endl;
+
+if (Month == 12 && Month <=3)
+    if (day <=22)
+        cout << "It is now winter! ❄️☃️"<<endl;
+
+
+
+
 
 
 
---
+++ Diaby_H_Homework_2.cpp
@@ -0,0 +1,3 @@
+// Halimatou Diaby
+//Lecture Four (If statements)
+//10/1/24
